747fb5a0a1f6098a72989a6e648ee320610ffff3
Toy Bonaparte Chan <50433565+BonaparteChan@users.noreply.github.com>
2019-07-23 15:32:24 +0800

    Update 10.py

--- code/10.py
+++ code/10.py
@@ -2,7 +2,29 @@
 # ## practice_square.py
 
 # 请在这里写下你的答案（参考答案在10-a.py中）
+#Hollow My name is Toy Bonaparte Chan，WeChat Nickname is Remember me
+#各位老师同学们好，我是陈
 
+import turtle
+#引入turtle 模块库model
 
+amy=turtle.Turtle()
+#create a turtle object 
+#创建一个turtle对象，并赋值给amy
 
+amy.color('yellow')
+#amy的颜色是黄色
+#amy's color yellow
 
+amy.forward(100)
+amy.right(90)
+#amy向前走100像素，往右走90度
+
+amy.forward(100)
+amy.right(90)
+#amy向前走100像素，往右走90度
+
+amy.forward(100)
+amy.right(90)
+#amy向前走100像素，往右走90度
+amy.forward(100)
